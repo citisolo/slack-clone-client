201d75994014727cfaa068ea530e6abc2558a508
Kwadwo Amankwa <citisolo@gmail.com>
2019-04-09 10:59:01 +0100

    Sending, Verifying and Refreshing JWT tokens in Node.js

---
+++ src/routes/CreateTeam.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Form, Container, Header, Input, Button, Message } from 'semantic-ui-react';
+import { gql, graphql } from 'react-apollo';
+
+class CreateTeam extends React.Component {
+  constructor(props) {
+    super(props);
+    extendObservable(this, {
+      name: '',
+      errors: {},
+    });
+  }
+
+  onSubmit = async () => {
+    this.errors = {};
+    const { name } = this;
+    const res = await this.props.mutate({
+      variables: { name },
+    });
+    const { ok, errors } = res.data.createTeam;
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
+      });
+      this.errors = err;
+    }
+  }
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this[name] = value;
+  }
+
+  render() {
+    const { name, errors: { nameError } } = this;
+    const errorList = [];
+
+    if (nameError) {
+      errorList.push(nameError);
+    }
+
+    return (
+      <Container text>
+        <Form>
+          <Form.Field>
+            <Header as="h2">Create a Team</Header>
+          </Form.Field>
+          <Form.Field error={!!nameError}>
+            <Input
+              name="name"
+              onChange={this.onChange}
+              value={name}
+              placeholder="Name"
+              fluid
+            />
+          </Form.Field>
+          <Form.Field>
+            <Button onClick={this.onSubmit}>Login</Button>
+          </Form.Field>
+        </Form>
+        { errorList.length !== 0 ? (
+          <Message
+            error
+            header="There was some errors with your submission"
+            list={errorList}
+          />
+        ) : null}
+      </Container>
+    );
+  }
+}
+
+const createTeamMutation = gql`
+  mutation($name: String!) {
+    createTeam(name: $name){
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(createTeamMutation)(observer(CreateTeam));
