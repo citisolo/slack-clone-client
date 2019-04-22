ffb6c27e858624bf99827321ae08854be195b752
Kwadwo Amankwa <citisolo@gmail.com>
2019-04-22 12:47:52 +0100

    Added message Container

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -3,9 +3,27 @@ import Messages from '../components/Messages';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import { Comment } from 'semantic-ui-react'
+
 const MessageContainer = ({ data: { loading, messages } }) => (loading ? null : (
   <Messages>
-    {JSON.stringify(messages)}
+    <Comment.Group>
+      {messages.map(m => (
+        <Comment key={`${m.id}-message`}>
+          <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+          <Comment.Content>
+            <Comment.Author as='a'>{m.user.username}</Comment.Author>
+            <Comment.Metadata>
+              <div>Today at {m.created_at}</div>
+            </Comment.Metadata>
+            <Comment.Text>{m.text}</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      ))}
+    </Comment.Group>
   </Messages>
 ));
 
@@ -16,7 +34,7 @@ const messagesQuery = gql`
       user {
         username
       }
-      createdAt
+      created_at
     }
   }
 `;
